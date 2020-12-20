154a34745e64e0ffec2c33baaa0a5c02af3ee712
Devin <drsingh2518@icloud.com>
2020-12-20 08:51:17 -0600

    changed cpu fields, added memory access

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,61 +1,71 @@
 #[allow(non_snake_case)]
-pub struct Chip8 {
+pub struct CPU {
     // Registers
-    V1: u8,
-    V2: u8,
-    V3: u8,
-    V4: u8,
-    V5: u8,
-    V6: u8,
-    V7: u8,
-    V8: u8,
-    V9: u8,
-    VA: u8,
-    VB: u8,
-    VC: u8,
-    VD: u8,
-    VE: u8,
-    /********************************************/
-    // Shouldn't be set by programs, used by certain instructions
-    VF: u8,
+    pub V: [u8; 16],
     /********************************************/
     // Stores memory addresses
-    I: u16,
+    pub I: u16,
     /********************************************/
     // Usually zero, decrement by 60hz when not
-    DT: u8,
-    ST: u8,
+    pub DT: u8,
+    pub ST: u8,
     /********************************************/
     // Program Counter
-    PC: u16,
+    pub PC: u16,
     // Stack Pointer
-    SP: u8,
+    pub SP: u8,
+    pub stack: [u16; 16],
+    pub ram: [u8; 4096],
 }
 
-impl Chip8 {
+impl CPU {
     pub fn new() -> Self {
-        Chip8 {
-            V1: 0x00,
-            V2: 0x00,
-            V3: 0x00,
-            V4: 0x00,
-            V5: 0x00,
-            V6: 0x00,
-            V7: 0x00,
-            V8: 0x00,
-            V9: 0x00,
-            VA: 0x00,
-            VB: 0x00,
-            VC: 0x00,
-            VD: 0x00,
-            VE: 0x00,
-            VF: 0x00,
+        let mut ram = [0x00; 4096];
+        ram[0..80].copy_from_slice(&[
+            0xF0, 0x90, 0x90, 0x90, 0xF0, /* 0 */
+            0x20, 0x60, 0x20, 0x20, 0x70, /* 1 */
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, /* 2 */
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, /* 3 */
+            0x90, 0x90, 0xF0, 0x10, 0x10, /* 4 */
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, /* 5 */
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, /* 6 */
+            0xF0, 0x10, 0x20, 0x40, 0x40, /* 7 */
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, /* 8 */
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, /* 9 */
+            0xF0, 0x90, 0xF0, 0x90, 0x90, /* A */
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, /* B */
+            0xF0, 0x80, 0x80, 0x80, 0xF0, /* C */
+            0xE0, 0x90, 0x90, 0x90, 0xE0, /* D */
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, /* E */
+            0xF0, 0x80, 0xF0, 0x80, 0x80, /* F */
+        ]);
+        CPU {
+            V: [0; 16],
             I: 0x0000,
             DT: 0x00,
             ST: 0x00,
             PC: 0x0000,
-            SP: 0x00,
+            // Points to topmost level of stack
+            SP: 0xFF,
+            stack: [0x0000; 16],
+            ram,
         }
+    }
+
+    pub fn evaluate_opcode(&mut self) {
+        let opcode = self.read_memory(self.PC);
+        todo!();
+    }
+
+    // u16 because of PC size
+    pub fn read_memory(&self, addr: u16) -> u16 {
+        /* Take value at first byte as first nibble */
+        /* Add value at second byte as second nibble */
+        (self.ram[addr as usize] as u16) << 8 | (self.ram[addr as usize + 1] as u16)
+    }
+
+    pub fn write_memory(&mut self, addr: u8, value: u8) {
+        self.ram[addr as usize] = value;
     }
 
     // Return from subroutine
@@ -69,4 +79,51 @@ impl Chip8 {
     }
 
     pub fn CALL(&mut self, addr: u16) {}
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_mem_read() {
+        let cpu = CPU::new();
+
+        assert_eq!(cpu.read_memory(0x00), 0xF090);
+        assert_eq!(cpu.read_memory(0x01), 0x9090);
+        assert_eq!(cpu.read_memory(0x02), 0x9090);
+        assert_eq!(cpu.read_memory(0x03), 0x90F0);
+        assert_eq!(cpu.read_memory(0x04), 0xF020);
+        assert_eq!(cpu.read_memory(0x05), 0x2060);
+        assert_eq!(cpu.read_memory(0x06), 0x6020);
+        assert_eq!(cpu.read_memory(0x07), 0x2020);
+        assert_eq!(cpu.read_memory(0x08), 0x2070);
+        assert_eq!(cpu.read_memory(0x09), 0x70F0);
+    }
+
+    #[test]
+    fn test_mem_write() {
+        let mut cpu = CPU::new();
+        cpu.write_memory(0x00, 0xFF);
+        cpu.write_memory(0x01, 0xFF);
+        cpu.write_memory(0x02, 0xFF);
+        cpu.write_memory(0x03, 0xFF);
+        cpu.write_memory(0x04, 0xFF);
+        cpu.write_memory(0x05, 0xFF);
+        cpu.write_memory(0x06, 0xFF);
+        cpu.write_memory(0x07, 0xFF);
+        cpu.write_memory(0x08, 0xFF);
+        cpu.write_memory(0x09, 0xFF);
+
+        assert_eq!(cpu.read_memory(0x00), 0xFFFF);
+        assert_eq!(cpu.read_memory(0x01), 0xFFFF);
+        assert_eq!(cpu.read_memory(0x02), 0xFFFF);
+        assert_eq!(cpu.read_memory(0x03), 0xFFFF);
+        assert_eq!(cpu.read_memory(0x04), 0xFFFF);
+        assert_eq!(cpu.read_memory(0x05), 0xFFFF);
+        assert_eq!(cpu.read_memory(0x06), 0xFFFF);
+        assert_eq!(cpu.read_memory(0x07), 0xFFFF);
+        assert_eq!(cpu.read_memory(0x08), 0xFFFF);
+        assert_eq!(cpu.read_memory(0x09), 0xFFF0);
+    }
 }
